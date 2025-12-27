2c225edc6f74b8cc21d621e4c27a6ddaaa63df23
LSM-Shamrock <lsm.shamrock@gmail.com>
2025-12-27 21:47:08 +0900

    15웨이브에서 뒷배경 변할때 부드럽게 변하도록 효과 추가

--- Assets/@Scripts/Controller/Enemy/Background_Ship.cs
+++ Assets/@Scripts/Controller/Enemy/Background_Ship.cs
@@ -3,13 +3,23 @@ using UnityEngine;
 
 public class Background_Ship : BaseController
 {
-    SpriteRenderer _sr;
+    private SpriteRenderer _sr;
+    
     protected override void Start()
     {
         _sr = GetComponent<SpriteRenderer>();
     }
+    
     protected override void Update()
     {
-        _sr.enabled = Manager.Game.wave == 15;
+        if (Manager.Game.wave == 15)
+        {
+            if (_sr.color.a < 1f)
+                _sr.AddAlpha(Time.deltaTime / 0.5f);
+            else
+                _sr.SetAlpha(1f);
+        }
+        else
+            _sr.SetAlpha(0f);
     }
 }
