3aabe8d80808179bb505081324cde30cdb32fb84
LSM-Shamrock <lsm.shamrock@gmail.com>
2025-12-27 12:47:54 +0900

    스킬 쿨타임 중일 때 ui모양 변경 및 드래그 막음

--- Assets/@Scripts/UI/Play/MobileControlUI/SkillJoystickUI.cs
+++ Assets/@Scripts/UI/Play/MobileControlUI/SkillJoystickUI.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SkillJoystickUI : UIBase, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
-    private ChildKey<RectTransform> Boddy = new(nameof(Boddy));
-    private ChildKey<RectTransform> Handle = new(nameof(Handle));
+    private ChildKey<Image> Boddy = new(nameof(Boddy));
+    private ChildKey<Image> Handle = new(nameof(Handle));
 
     private Vector3 _defaultPosition;
     private float _joystickRadius;
@@ -14,36 +15,48 @@ public class SkillJoystickUI : UIBase, IPointerDownHandler, IPointerUpHandler, I
         Init();
     }
 
+    private void Update()
+    {
+        bool isCooltime = Manager.Game.SpecialSkillCooltime > 0;
+
+        GetChild(Boddy).enabled = !isCooltime;
+        GetChild(Handle).color = isCooltime ? new Color(0.4f,0.4f,0f) : Color.yellow;
+    }
+
     private void Init()
     {
         BindChild(
         Boddy,
         Handle);
 
-        _defaultPosition = GetChild(Boddy).position;
-        _joystickRadius = GetChild(Boddy).sizeDelta.y / 2f;
+        _defaultPosition = GetChild(Boddy).transform.position;
+        _joystickRadius = GetChild(Boddy).rectTransform.sizeDelta.y / 2f;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         Vector3 worldPosition = Manager.Object.MainCamera.ScreenToWorldPoint(eventData.position);
-        GetChild(Boddy).position = worldPosition;
+        GetChild(Boddy).transform.position = worldPosition;
 
         Manager.Input.isOnSkillJoystick = true;
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        GetChild(Boddy).position = _defaultPosition;
-        GetChild(Handle).localPosition = Vector3.zero;
+        GetChild(Boddy).transform.position = _defaultPosition;
+        GetChild(Handle).transform.localPosition = Vector3.zero;
 
         Manager.Input.isOnSkillJoystick = false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Transform body = GetChild(Boddy);
-        Transform handle = GetChild(Handle);
+        bool isCooltime = Manager.Game.SpecialSkillCooltime > 0;
+        if (isCooltime)
+            return;
+
+        Transform body = GetChild(Boddy).transform;
+        Transform handle = GetChild(Handle).transform;
 
         Vector3 worldPos = Manager.Object.MainCamera.ScreenToWorldPoint(eventData.position);
         Vector3 dragVec = worldPos - body.position;
